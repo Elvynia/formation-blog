16159c76bbaf774313cf1a3b2ca37f408e57b3a1
Elvynia <elvynia@gmail.com>
2018-07-18 10:15:01 +0200

    Gestion de l'affichage liste ou formulaire.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,17 @@ import { Article } from './article';
 export class AppComponent {
 	title: string;
 	articles: Array<Article>;
+	showList: boolean;
 
 	constructor() {
+		this.showList = true;
 		this.title = 'Bienvenue sur mon blog réalisé avec Angular 6.0.3 !';
 		this.articles = new Array();
 	}
 
 	handleCreate(article: Article) {
 		this.articles.push(article);
-		console.log('Un article à été ajouté : ', article);
+		this.showList = true;
 	}
 
 	deleteArticle(id: number) {
@@ -29,7 +31,7 @@ export class AppComponent {
 	}
 
 	private updateList(id: number, article?: Article) {
-		let index = this.articles.findIndex((a) => a.id === id);
+		let index = this.articles.findIndex((truc) => truc.id === id);
 		if (index >= 0) {
 			if (article) {
 				this.articles.splice(index, 1, article);
