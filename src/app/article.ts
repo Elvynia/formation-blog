78c4fb02b58622161ccc17c919766188b7876368
Elvynia <elvynia@gmail.com>
2018-07-19 12:17:09 +0200

    Redéfinition de la méthode toString().

--- src/app/article.ts
+++ src/app/article.ts
@@ -2,4 +2,8 @@ export class Article {
     id: number;
     title: string;
     description: string;
+
+    public toString = () : string => {
+        return `[id=${this.id}, title=${this.title}]`;
+    }
 }
