9d5ee2b85fd88277988342ddb7f98263ef81ca3d
Elvynia <elvynia@gmail.com>
2018-07-20 14:59:52 +0200

    Fin implémentation ViewEditComponent.

--- src/app/view-edit/view-edit.component.ts
+++ src/app/view-edit/view-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Article } from '../article';
 import { ArticleService } from '../article.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
 	selector: 'blog-view-edit',
@@ -12,7 +13,7 @@ export class ViewEditComponent implements OnInit {
 	article: Article;
 
 	constructor(private articleService: ArticleService,
-		private route: ActivatedRoute) { }
+		private route: ActivatedRoute, private location: Location) { }
 
 	ngOnInit() {
 		// FIXME: Mauvaise pratique -> subscribe dans subscribe...
@@ -25,6 +26,29 @@ export class ViewEditComponent implements OnInit {
 				this.article = undefined;
 			}
 		});
+	}
+
+	create(article: Article) {
+		this.articleService.create(article)
+			.subscribe({
+				next: (newArticle) => console.log(`Article ${newArticle} créé !`),
+				error: (errorMessage) => console.log(`Impossible de créer l'article ${article} : ${errorMessage}`),
+				complete: () => {
+					console.log('Création du nouvel article terminée avec succès !');
+					this.location.back();
+				}
+			});
+	}
+
+	update(article: Article) {
+		this.articleService.update(article)
+			.subscribe({
+				complete: () => {
+					console.log(`Article d'id ${article.id} mis à jour avec succès`);
+					this.location.back();
+				},
+				error: (message) => console.log(`Impossible de mettre à jour l'article : ${message}`)
+			});
 	}
 
 }
